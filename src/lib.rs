1b1ad4c5e90e6c570efb63c98afec9e2a75f742e
Tobias Bucher <tobiasbucher5991@gmail.com>
2015-02-25 11:51:43 +0100

    Add the `unwrap_or_return!` macro
    This is a short-circuiting macro for the pervasive `Option` type.

--- src/lib.rs
+++ src/lib.rs
@@ -11,3 +11,23 @@ pub mod mem;
 pub mod format;
 pub mod syntax_ext;
 pub mod matches;
+
+/// Unwraps an `Option` or returns from the function with the specified return
+/// value.
+///
+/// Can be used on `Result`s by first calling `.ok()` or `.err()` on them.
+///
+/// # Examples
+///
+/// ```notest
+/// # // FIXME: Import the macro here, currently the compiler complains about
+/// # // missing `unwrap_or_return!`.
+/// fn take_pair<I:Iterator>(iter: &mut I) -> Option<(<I as Iterator>::Item, <I as Iterator>::Item)> {
+///    let first = unwrap_or_return!(iter.next(), None);
+///    Some((first, unwrap_or_return!(iter.next(), None)))
+/// }
+/// ```
+#[macro_export]
+macro_rules! unwrap_or_return {
+    ($e:expr, $r:expr) => (match $e { Some(e) => e, None => return $r, })
+}
