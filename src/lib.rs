2c6c17092ec53c0f46bdd4ddac6ad88005f99d5a
Keegan McAllister <mcallister.keegan@gmail.com>
2015-03-03 12:55:20 -0800

    Add a do-while loop macro

--- src/lib.rs
+++ src/lib.rs
@@ -31,3 +31,39 @@ pub mod matches;
 macro_rules! unwrap_or_return {
     ($e:expr, $r:expr) => (match $e { Some(e) => e, None => return $r, })
 }
+
+/// Do-while loop.
+///
+/// # Examples
+///
+/// ```
+/// # #[macro_use] extern crate mac;
+/// # fn main() {
+/// let mut i = 0;
+/// let mut n = 0;
+///
+/// do_while!({
+///     n += i;
+///     i += 1;
+/// } while i < 5);
+///
+/// assert_eq!(n, 10);
+/// # }
+/// ```
+///
+/// The loop always executes at least once.
+///
+/// ```
+/// # #[macro_use] extern crate mac;
+/// # fn main() {
+/// let mut ran = false;
+/// do_while!({ ran = true } while false);
+/// assert!(ran);
+/// # }
+/// ```
+#[macro_export]
+macro_rules! do_while {
+    ($body:block while $condition:expr) => {
+        while { $body; $condition } { }
+    }
+}
